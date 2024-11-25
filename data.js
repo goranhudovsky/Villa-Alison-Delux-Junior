9aa968bc2c135299bd6e5e9e1f4a58308657c4c7
Centar 360 <60780694+goranhudovsky@users.noreply.github.com>
2024-11-25 13:57:00 +0100

    Junior

---
+++ data.js
@@ -0,0 +1,599 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-terasa",
+      "name": "Terasa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": -0.6963633726793148,
+        "pitch": -0.0608212485055617,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0006238945965204579,
+          "pitch": 0.08774580837856405,
+          "rotation": 0,
+          "target": "2-kuninja---blagavaonica"
+        },
+        {
+          "yaw": -1.3620534852246777,
+          "pitch": 0.08242491896101001,
+          "rotation": 0,
+          "target": "1-bazen-1"
+        },
+        {
+          "yaw": -2.837433924563193,
+          "pitch": 0.07475649638871928,
+          "rotation": 0,
+          "target": "11-ulaz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bazen-1",
+      "name": "Bazen 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.4897607594882345,
+        "pitch": 0.02787640556504911,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2082304368902257,
+          "pitch": 0.027404249050123397,
+          "rotation": 0,
+          "target": "0-terasa"
+        },
+        {
+          "yaw": 1.6882083948817517,
+          "pitch": 0.032186826110242706,
+          "rotation": 0,
+          "target": "11-ulaz"
+        },
+        {
+          "yaw": -0.16315378404701164,
+          "pitch": 0.026635633415244442,
+          "rotation": 0,
+          "target": "9-bazen-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kuninja---blagavaonica",
+      "name": "Kuninja - Blagavaonica",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": -1.9815342694378373,
+        "pitch": 0.10897140357246471,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7796711584619445,
+          "pitch": -0.02300700249685228,
+          "rotation": 0,
+          "target": "0-terasa"
+        },
+        {
+          "yaw": 2.782500076196458,
+          "pitch": 0.09633937963751649,
+          "rotation": 0,
+          "target": "3-koridor"
+        },
+        {
+          "yaw": 1.1154327570159257,
+          "pitch": 0.06155905126525951,
+          "rotation": 0,
+          "target": "4-dnevni-boravak"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-koridor",
+      "name": "Koridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.9376208119258571,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6252352381447803,
+          "pitch": 0.0336678987728547,
+          "rotation": 0,
+          "target": "4-dnevni-boravak"
+        },
+        {
+          "yaw": -0.2324620605263874,
+          "pitch": 0.06990371573995802,
+          "rotation": 0,
+          "target": "2-kuninja---blagavaonica"
+        },
+        {
+          "yaw": -2.0029116568900385,
+          "pitch": -0.00414440151139317,
+          "rotation": 0,
+          "target": "5-spavaonica-1"
+        },
+        {
+          "yaw": 2.0300047823423037,
+          "pitch": 0.05860755961333197,
+          "rotation": 0,
+          "target": "7-spavaonica-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dnevni-boravak",
+      "name": "Dnevni boravak",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0566515268150667,
+          "pitch": 0.05636933029225766,
+          "rotation": 0,
+          "target": "2-kuninja---blagavaonica"
+        },
+        {
+          "yaw": 0.2447647381901703,
+          "pitch": 0.039386494989566145,
+          "rotation": 0,
+          "target": "3-koridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-spavaonica-1",
+      "name": "Spavaonica 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 2.432931189109781,
+        "pitch": 0.09630031013380602,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6192474408479614,
+          "pitch": 0.0996995547040207,
+          "rotation": 0,
+          "target": "6-kupaonica-1"
+        },
+        {
+          "yaw": -2.351079047538361,
+          "pitch": 0.09189450840645996,
+          "rotation": 0,
+          "target": "3-koridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kupaonica-1",
+      "name": "Kupaonica 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 1.9295347013306952,
+        "pitch": -0.03531100616074134,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3888451255522831,
+          "pitch": 0.02898208039936989,
+          "rotation": 0,
+          "target": "5-spavaonica-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-spavaonica-2",
+      "name": "Spavaonica 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.13891396087306873,
+        "pitch": 0.012671093438658687,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5662572560262227,
+          "pitch": 0.015109918497051567,
+          "rotation": 0,
+          "target": "3-koridor"
+        },
+        {
+          "yaw": -2.874934276872718,
+          "pitch": 0.1128438158597369,
+          "rotation": 0,
+          "target": "8-kupaonica-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-kupaonica-2",
+      "name": "Kupaonica 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 2.1207970665897466,
+        "pitch": 0.12268908185124161,
+        "fov": 1.1944249545268704
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0650051720004114,
+          "pitch": -0.012563942060449307,
+          "rotation": 0,
+          "target": "7-spavaonica-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bazen-2",
+      "name": "Bazen 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 3.131293497992681,
+        "pitch": 0.07019914055475773,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.806387855220679,
+          "pitch": 0.06006226580383078,
+          "rotation": 0,
+          "target": "1-bazen-1"
+        },
+        {
+          "yaw": 0.6719736500321076,
+          "pitch": 0.05374048195509751,
+          "rotation": 0,
+          "target": "10-spa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-spa",
+      "name": "Spa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.32769447180301015,
+        "pitch": 0.0202737495018539,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5459592416992187,
+          "pitch": 0.011316889290739596,
+          "rotation": 0,
+          "target": "9-bazen-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-ulaz",
+      "name": "Ulaz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.008905522008923583,
+        "pitch": -0.0025342186877317374,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12952716648731766,
+          "pitch": -0.04705819386952115,
+          "rotation": 0,
+          "target": "0-terasa"
+        },
+        {
+          "yaw": -0.8525594078190082,
+          "pitch": 0.02548655253862897,
+          "rotation": 0,
+          "target": "1-bazen-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Villa Alison Delux Junior",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
